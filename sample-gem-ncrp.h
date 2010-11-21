579cca462adbb23884a7d99ab5e12b21fc2e3b9d
Joseph Reisinger <joeraii@cs.utexas.edu>
2010-11-20 19:22:06 -0600

    apache

--- sample-gem-ncrp.h
+++ sample-gem-ncrp.h
@@ -1,3 +1,18 @@
+/*
+   Copyright 2010 Joseph Reisinger
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 // This implements (I think) the GEM distributed hLDA, which has an infinitely
 // deep tree, where each node can have (possibly) infinitely many branches.
 // There are some issues with implementing this sampler that I (and others)
